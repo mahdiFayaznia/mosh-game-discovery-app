efc45561e511e4fb3b43e8193b44606ce0e28de8
Mahdi Fayaznia <mahdifayaznia@gmail.com>
2024-01-15 21:34:40 +0330

    Add search input

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -11,7 +11,7 @@ const ColorModeSwitch = () => {
         onChange={toggleColorMode}
       /> */}
       <Switch isChecked={colorMode === "dark"} onChange={toggleColorMode} />
-      <Text>Dark Mode</Text>
+      <Text whiteSpace="nowrap">Dark Mode</Text>
     </HStack>
   );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/react.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px 20px">
-      <Image src={logo} boxSize="60px" />
+      <Image src={logo} boxSize="35px" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
